8303f4c844ec554d77461b0c05b8cf0bd70337c7
Anthony Rezzonico <anthonyrezz@gmail.com>
2021-08-09 21:18:50 -0600

    Initial setup of the pom.xml for the application

---
+++ server/src/main/java/com/springsrescuemisson/kenneltracker/KennelTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.springsrescuemisson.kenneltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KennelTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KennelTrackerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/springsrescuemisson/kenneltracker/KennelTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsrescuemisson.kenneltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KennelTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
