4829188f586de3cf91d73b904d567d94d6b084da
Anthony Rezzonico <anthonyrezz@gmail.com>
2021-09-28 12:15:55 -0600

    Updating Dockerfile to contain data.sql

---
+++ postgres-db/data.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS client(
+	id NUMBER PRIMARY KEY,
+	firstName VARCHAR(50) NOT NULL,
+	lastName  VARCHAR(50) NOT NULL,
+	phoneNumber VARCHAR(50) NOT NULL,
+	emergencyContact VARCHAR(50),
+	shelterType VARCHAR(50),
+	bedNumber SMALLINT
+);
+
+CREATE TABLE IF NOT EXISTS PET(
+	id NUMBER PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	type VARCHAR(50) NOT NULL,
+	breed VARCHAR(50),
+	gender VARCHAR(10),
+	color VARCHAR(50),
+	kennelNumber SMALLINT,
+	restricted BOOLEAN
+);
