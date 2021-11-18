51905e3a7651751bf7b8c4d1e81206998e142069
Anthony Rezzonico <anthonyrezz@gmail.com>
2021-11-17 17:25:13 -0700

    Added the ability to search through the kennels

--- kennel-viewer/src/app/tab2/tab2.page.ts
+++ kennel-viewer/src/app/tab2/tab2.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { KennelService } from '../kennel.service';
 import { MOCK_KENNEL_LAYOUT } from '../mock-kennel';
 import { Kennel } from '../models/Kennel';
+import { Pet } from '../models/Pet';
 
 @Component({
   selector: 'app-tab2',
@@ -11,13 +12,27 @@ import { Kennel } from '../models/Kennel';
 export class Tab2Page implements OnInit{
 
   kennelLayout ?: Kennel[];
+  kennelFilterMatch: Kennel[];
+  petFilter?: string;
 
   constructor(
     public kennelService: KennelService
   ) {}
 
   ngOnInit(): void {
-    this.kennelLayout = MOCK_KENNEL_LAYOUT;
+    this.kennelFilterMatch = MOCK_KENNEL_LAYOUT;
+  }
+
+  search(value: string) {
+    console.log(value);
+    this.petFilter = value;
+    this.kennelFilterMatch = this.petFilter ? this.performFilter(this.petFilter) : this.kennelService.kennelLayout;
+  }
+
+  performFilter(petFilter: string) {
+    petFilter = petFilter.toLocaleLowerCase();
+    return this.kennelService.kennelLayout
+      .filter((kennel: Kennel) => kennel.pet.name.toLocaleLowerCase().includes(petFilter));
   }
 
 }
